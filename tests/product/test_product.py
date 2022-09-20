85f9e53d59d084db242e3dc4ccb4c4727acf3891
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-09-20 18:24:07 -0300

    chore: teste cria produtos

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    result = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+
+    assert result.id == 1
+    assert result.nome_do_produto == "Nicotine Polacrilex"
+    assert result.nome_da_empresa == "Target Corporation"
+    assert result.data_de_fabricacao == "2021-02-18"
+    assert result.data_de_validade == "2023-09-17"
+    assert result.numero_de_serie == "CR25 1551 4467 2549 4402 1"
+    assert result.instrucoes_de_armazenamento == "instrucao 1"
