125abf0b03f1bf9362539803a241ba85ba1daa18
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-09-20 18:46:24 -0300

    chore: teste relatório produtos

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    result = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "na geladeira",
+    )
+
+    expected = (
+        f"O produto Nicotine Polacrilex"
+        f" fabricado em 2021-02-18"
+        f" por Target Corporation com validade"
+        f" até 2023-09-17"
+        f" precisa ser armazenado na geladeira."
+    )
+
+    assert str(result) == expected
